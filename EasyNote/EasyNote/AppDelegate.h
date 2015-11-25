788fc57d82e51e4dce15169a01556c80289ac1cf
darren90 <1005052145@qq.com>
2015-11-25 22:54:48 +0800

    init project

---
+++ EasyNote/EasyNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyNote
+//
+//  Created by Tengfei on 15/11/25.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyNote/EasyNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyNote
+//
+//  Created by Tengfei on 15/11/25.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
