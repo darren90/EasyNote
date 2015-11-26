2abca1229d1eedc45b53073da022ab3afc3b7e33
darren90 <1005052145@qq.com>
2015-11-26 23:52:57 +0800

    初始化第三方服务

--- EasyNote/EasyNote/AppDelegate.m
+++ EasyNote/EasyNote/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import <Bugly/CrashReporter.h>
+#import "MobClick.h"
 
 @interface AppDelegate ()
 
@@ -14,12 +16,40 @@
 
 @implementation AppDelegate
 
+////设置是否允许推送为允许
+//NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+////    if (self.item.key) {
+//[defaults setBool:YES forKey:KSettingPush];
+//[defaults setBool:YES forKey:KSettingUse3G];
+//[defaults synchronize];
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [[CrashReporter sharedInstance] installWithAppId:KBuglyAPPID];
+    
+    [self umengTrack];//友盟的方法本身是异步执行，所以不需要再异步调用
+    
     return YES;
 }
-
+- (void)umengTrack {
+    //    [MobClick setCrashReportEnabled:NO]; // 如果不需要捕捉异常，注释掉此行
+    [MobClick setLogEnabled:NO];  // 打开友盟sdk调试，注意Release发布时需要注释掉此行,减少io消耗
+    [MobClick setAppVersion:XcodeAppVersion]; //参数为NSString * 类型,自定义app版本信息，如果不设置，默认从CFBundleVersion里取
+    //
+    [MobClick startWithAppkey:KUmegnAppKey reportPolicy:(ReportPolicy) REALTIME channelId:nil];
+    //   reportPolicy为枚举类型,可以为 REALTIME, BATCH,SENDDAILY,SENDWIFIONLY几种
+    //   channelId 为NSString * 类型，channelId 为nil或@""时,默认会被被当作@"App Store"渠道
+    
+//    [MobClick updateOnlineConfig];  //在线参数配置
+    
+    //    1.6.8之前的初始化方法
+    //    [MobClick setDelegate:self reportPolicy:REALTIME];  //建议使用新方法
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onlineConfigCallBack:) name:UMOnlineConfigDidFinishedNotification object:nil];
+}
+- (void)onlineConfigCallBack:(NSNotification *)note {
+    
+    NSLog(@"online config has fininshed and note = %@", note.userInfo);
+}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -41,5 +71,6 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
 
 @end
