f48ebb3a51c2429b50e4e29e3506826db464ac4f
darren90 <1005052145@qq.com>
2015-12-20 18:26:06 +0800

    由于1.1版本数据库升级，导致1.0版本不兼容问题的修复

---
+++ EasyNote/EasyNote/Class/Main/Tools/EasyNoteTools.h
@@ -0,0 +1,14 @@
+//
+//  EasyNoteTools.h
+//  EasyNote
+//
+//  Created by Tengfei on 15/12/20.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EasyNoteTools : NSObject
+
++(void)isNewVersion;
+@end
---
+++ EasyNote/EasyNote/Class/Main/Tools/EasyNoteTools.m
@@ -0,0 +1,37 @@
+//
+//  EasyNoteTools.m
+//  EasyNote
+//
+//  Created by Tengfei on 15/12/20.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import "EasyNoteTools.h"
+
+@implementation EasyNoteTools
+
+
++(void)isNewVersion
+{
+    //取出沙盒中存储的上一次使用的版本号
+    //    NSString *key = @"CFBundleVersion";//build
+    NSString *key = @"CFBundleShortVersionString";//version
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *lastVersion = [defaults stringForKey:key];
+    
+    //获得当前软件的版本号
+    NSString *currentVersion = [NSBundle mainBundle].infoDictionary[key];
+    if([currentVersion isEqualToString:lastVersion]){
+        
+    }else{//新版本
+        
+        //存储新版本
+        [defaults setObject:currentVersion forKey:key];
+        [defaults synchronize];
+    }
+}
+
+
+
+
+@end
